79c4ae8bdd82bea1dfc719deaaf6c3485768fdb6
Meowzszs <tkiddy231@gmail.com>
2023-12-02 15:34:59 -0500

    Added quick Sort

---
+++ Quick_Sort.cpp
@@ -0,0 +1,5 @@
+//
+// Created by User on 12/2/2023.
+//
+
+#include "Quick_Sort.h"
---
+++ Quick_Sort.h
@@ -0,0 +1,14 @@
+//
+// Created by User on 12/2/2023.
+//
+
+#ifndef PROJECT3_AVAV_QUICK_SORT_H
+#define PROJECT3_AVAV_QUICK_SORT_H
+
+
+class Quick_Sort {
+
+};
+
+
+#endif //PROJECT3_AVAV_QUICK_SORT_H
