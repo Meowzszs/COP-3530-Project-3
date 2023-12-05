fa18652ba0b593c4107f489cb09f6967bd9ae7b8
AvaVellozzi <arvello45@gmail.com>
2023-12-04 19:20:22 -0500

    updated comments in food_maps file

--- food_maps.h
+++ food_maps.h
@@ -1,16 +1,21 @@
 #include <vector>
 #include <string>
 
+// Definition of the FoodItem class
 class FoodItem {
 public:
+    // Attributes of a FoodItem
     std::string name;
     std::vector<double> micronutrients;
 
+    // Default constructor initializing attributes
     FoodItem() : name(""), micronutrients({}) {}
 
+    // Parameterized constructor for creating a FoodItem with specified name and micronutrients
     FoodItem(std::string& name, std::vector<double>& micronutrients)
             : name(name), micronutrients(micronutrients) {}
 
+    // Overloaded equality operator for comparing FoodItems based on their names
     bool operator==(const FoodItem &other) const {
         return name == other.name;
     }
